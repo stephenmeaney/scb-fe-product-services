d368f7102d80ed411fd2abd0d6e875b74527333b
Stephen Meaney <smeaney@solstice.com>
2018-09-20 11:31:08 -0500

    Fix relationships for insert between Account and Address, Order and OrderLineItem. Implement Product Feign client for OrderLineItem

--- src/main/java/com/stephenmeaney/services/ProductServicesApplication.java
+++ src/main/java/com/stephenmeaney/services/ProductServicesApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableFeignClients
 @EnableDiscoveryClient
 public class ProductServicesApplication {
 
