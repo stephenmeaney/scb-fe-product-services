f351d4ea6758fc3901cc8a81eadb9e53c524cfb5
Stephen Meaney <smeaney@solstice.com>
2018-09-21 12:55:06 -0500

    Before review

---
+++ src/test/java/com/stephenmeaney/services/product/ProductTestSuite.java
@@ -0,0 +1,13 @@
+package com.stephenmeaney.services.product;
+
+import com.stephenmeaney.services.product.api.ProductControllerTest;
+import com.stephenmeaney.services.product.data.repository.ProductRepositoryIntegrationTest;
+import com.stephenmeaney.services.product.data.repository.ProductRepositoryTest;
+import com.stephenmeaney.services.product.service.ProductServiceTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses ({ProductControllerTest.class, ProductRepositoryTest.class, ProductRepositoryIntegrationTest.class, ProductServiceTest.class})
+public class ProductTestSuite {
+}
